fe5d5d985810556f20fe728367686be94b826fee
Remi Mendoza <mendozaremi@gmail.com>
2020-07-06 16:22:04 -0400

    completed project

---
+++ src/ui/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import spinner from '../img/spinner.gif'
+
+const Spinner = () => {
+  return (
+      <img src={spinner} style={{ width: '150px', margin: 'auto', display: 'block'}}
+      alt="loading" />
+  )
+}
+
+export default Spinner
