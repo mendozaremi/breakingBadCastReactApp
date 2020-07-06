7bf0b92215e5935746031a5abc9e5af3ee9dce37
Remi Mendoza <mendozaremi@gmail.com>
2020-07-06 16:34:50 -0400

    Updates

---
+++ precache-manifest.a9839282ac115c8f36b41289e3e05997.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5aafcdbe03f2e0e55ca663eebf833d",
+    "url": "/breakingBadCastReactApp/index.html"
+  },
+  {
+    "revision": "4fa474f0d4725193c19a",
+    "url": "/breakingBadCastReactApp/static/css/main.77df35d5.chunk.css"
+  },
+  {
+    "revision": "428bf282b2d6fdad0db5",
+    "url": "/breakingBadCastReactApp/static/js/2.f8634446.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breakingBadCastReactApp/static/js/2.f8634446.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa474f0d4725193c19a",
+    "url": "/breakingBadCastReactApp/static/js/main.c9ca8c0d.chunk.js"
+  },
+  {
+    "revision": "8e9ea4fb3b2a008b34af",
+    "url": "/breakingBadCastReactApp/static/js/runtime-main.cba70dee.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breakingBadCastReactApp/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breakingBadCastReactApp/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breakingBadCastReactApp/static/media/spinner.235b9b3e.gif"
+  }
+]);
