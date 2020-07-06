3125b41235d207f89ea8d802725a5f076fb6525f
Remi Mendoza <mendozaremi@gmail.com>
2020-07-06 16:30:17 -0400

    added - gh-pages ready for deploy

--- src/components/Characters/CharacterGrid.js
+++ src/components/Characters/CharacterGrid.js
@@ -4,13 +4,13 @@ import Spinner from '../../ui/Spinner'
 
 const CharacterGrid = ({items, isLoading}) => {
   return isLoading ? (
- <Spinner />
-  ) : (
-    <section className="cards">
-      {items.map(item => (
-        <CharacterItem key={item.char_id} item={item}></CharacterItem>
-      ))}
-    </section>
+    <Spinner />
+    ) : (
+      <section className="cards">
+        {items.map(item => (
+          <CharacterItem key={item.char_id} item={item}></CharacterItem>
+        ))}
+      </section>
   )
 }
 
